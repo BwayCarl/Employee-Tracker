206eac0cf12389137c180bd094e0e673614dca3c
Carlos <bwaycarl@gmail.com>
2020-09-29 00:31:06 -0400

    Update password.

--- tracker.js
+++ tracker.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "NewYorkMets!1986", //Enter your MySQL password here.
+  password: "", //Enter your MySQL password here.
   database: "employees_db"
 });
 
